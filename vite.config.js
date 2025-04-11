fbce0413d21ea800d0486cbcb3f78bd4e390bac2
Kevin-MeepL <40576680+Kevin-MeepL@users.noreply.github.com>
2025-04-11 12:38:41 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/my-portfolio/",
-})
+  build: {
+    outDir: 'dist', // Ensure this is set if it's not the default
+  },
+});
